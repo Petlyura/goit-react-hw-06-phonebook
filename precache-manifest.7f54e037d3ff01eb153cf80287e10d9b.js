866b010d2b4a67337bb6f13693b2d749ded500fe
Yuriy Tymchyna <ytymchyna@gmail.com>
2021-07-01 16:12:50 +0300

    Updates

---
+++ precache-manifest.7f54e037d3ff01eb153cf80287e10d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0229c2e0c1e20ae62f499a3b7ba66f5",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "2827056a3cb771238eac",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.a9bd50d0.chunk.css"
+  },
+  {
+    "revision": "1ca57167baf202cb3c80",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.90c09005.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.90c09005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2827056a3cb771238eac",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.39d0c4e6.chunk.js"
+  },
+  {
+    "revision": "b5ed5ed338211b67838b",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.82a9593a.js"
+  }
+]);
